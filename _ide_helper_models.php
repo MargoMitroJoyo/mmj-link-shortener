6668289ee884e1e7345af97682923212325a39aa
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-05 09:31:31 +0700

    feat: add Eloquent model helper file for Link and User models

---
+++ _ide_helper_models.php
@@ -0,0 +1,95 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $title
+ * @property string|null $slug
+ * @property string|null $url
+ * @property string|null $description
+ * @property int $status
+ * @property string|null $expired_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereExpiredAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Link withoutTrashed()
+ */
+	class Link extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read mixed $breezy_session
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Jeffgreco13\FilamentBreezy\Models\BreezySession> $breezySessions
+ * @property-read int|null $breezy_sessions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Link> $links
+ * @property-read int|null $links_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
+ * @property-read int|null $permissions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Role> $roles
+ * @property-read int|null $roles_count
+ * @property-read mixed $two_factor_recovery_codes
+ * @property-read mixed $two_factor_secret
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User permission($permissions, $without = false)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User role($roles, $guard = null, $without = false)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User withoutPermission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User withoutRole($roles, $guard = null)
+ */
+	class User extends \Eloquent implements \Filament\Models\Contracts\FilamentUser {}
+}
+
